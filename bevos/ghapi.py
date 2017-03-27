1cabcc5e5c478120d802a48f8df150b43af22b83
Eric Douglass <edouglass@lbl.gov>
2017-03-27 12:46:34 -0700

    removing some prints from testing

--- bevos/ghapi.py
+++ bevos/ghapi.py
@@ -53,22 +53,12 @@ def endpoint_url(github_url: str, endpoint: str) -> str:
 def perform_release(owner: str, repo: str, tag: str, target_sha: str, token_path:str, description: str, dryrun: bool) -> GhResult:
     endpoint = make_release_endpoint(owner, repo)
     url = endpoint_url("https://api.github.com/", endpoint)
-    # print("POST {}".format(url))
+    print("POST {}".format(url))
     data = make_release_data(tag, target_sha, description, dryrun)
-    # print(json.dumps(data, indent=4))
+    print(json.dumps(data, indent=4))
     header = auth_header(token(token_path))
     print(header)
 
-    request = requests.Request(
-        method="post",
-        url=url,
-        data=json.dumps(data),
-        headers=header
-    )
-    print(request.method)
-    print(request.url)
-    print(request.headers)
-    print(request.data)
     response = requests.post(url, data=json.dumps(data), headers=header)
     print(response.text)
     return GhResult(response)
